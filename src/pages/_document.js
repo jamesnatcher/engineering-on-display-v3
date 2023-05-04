8698180798787feb3f30d07bfb54dd353006c466
James Natcher <jamesnatcher@gmail.com>
2023-05-04 15:08:55 -0800

    Adding Icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="icon" href="/307489854_10159790468510236_587772469651996448_n.jpg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
